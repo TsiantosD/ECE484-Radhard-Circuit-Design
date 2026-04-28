8a18fe323d649b0a76372cbcb451ca09e6c0774f
TsiantosD <tsiantos.d@gmail.com>
2026-04-28 22:24:03 +0300

    Change zip.sh to zip the second part of the project

--- zip.sh
+++ zip.sh
@@ -5,7 +5,7 @@ echo "Zipping Project..."
 echo "========================================"
 
 # Define the name of the output zip file
-ARCHIVE_NAME="Tsiantos_Kalousis_Tsimponidis-Project1_PartA.zip"
+ARCHIVE_NAME="Tsiantos_Kalousis_Tsimponidis-Project1_PartB.zip"
 
 # Remove the old archive if it already exists so we don't double-zip
 rm -f "$ARCHIVE_NAME"
@@ -15,13 +15,15 @@ rm -f "$ARCHIVE_NAME"
 # -x means exclude the following files/patterns
 zip -r "$ARCHIVE_NAME" . \
     -x "./Project1_partA.pdf" \
+    -x "./Project1_partB.pdf" \
     -x "./zip.sh" \
     -x "./$ARCHIVE_NAME" \
     -x "./outputs/*" \
     -x "./visualizer/venv/*" \
     -x "./visualizer/node_modules/*" \
     -x "./.git/*" \
-    -x "./.git"
+    -x "./.git" \
+    -x "./*.zip"
 
 echo ""
 echo "✅ Successfully created $ARCHIVE_NAME"
